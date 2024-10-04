971f3a84329c620a1f2451921235fe9a41ba2797
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2024-10-04 12:40:39 +0200

    Add inheritance from esphome component

--- components/ble_scanner/bleScanner.h
+++ components/ble_scanner/bleScanner.h
@@ -5,7 +5,7 @@ namespace jcc{ //jack custom components namespace
  * Class to represent a BLE device scanner. Detects nearby ble devices and allows to perform some operations.
  * Designed to read RSSI.
  */
-    class bleScanner{
+    class bleScanner: public esphome::Component{
         public:
         /**
          * 
