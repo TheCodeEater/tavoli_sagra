2abef30afaf955f2d6c818478f452575b9a981df
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2024-10-06 15:28:37 +0200

    cleanup

--- components/ble_scanner/bleScanner.h
+++ components/ble_scanner/bleScanner.h
@@ -25,7 +25,7 @@ namespace jcc{ //jack custom components namespace
         /**
          * Create empty device association
          */
-            bleScanner(): m_nearby_devices{}, m_nearby_devices_lastonline{}, m_last_timestamp{}{
+            bleScanner(): m_devices{}, m_scanComplete{false} {
 
             }
 
@@ -49,10 +49,7 @@ namespace jcc{ //jack custom components namespace
          * Add device with mac and rssi
          */
         void setDevice(std::string mac, int8_t rssi, uint16_t time){
-            /*m_nearby_devices[mac]=rssi; //match devices and rssi
-            m_nearby_devices_lastonline[mac]=time; //match device and last update
-            m_last_timestamp=time; //save last timestamp*/
-            //ESP_LOGD("set device",mac.c_str());
+
         }
         /**
          * Empty device list
@@ -64,39 +61,15 @@ namespace jcc{ //jack custom components namespace
          * Check if there are devices with rssi greater than the thd
          */
         bool checkNearbyDevices(int8_t threshold){
-            /*for(auto const& device : m_nearby_devices){//iterate over each detected device
-                if(device.second>threshold /*&& device.first=="4C:D0:19:38:D7:F7"){ //if the rssi is greater than the minimu, then turn on lights
-                    found_nearby=true; //flag as found, do not halt the loop. see below
-                    //ESP_LOGD("Nearby","yes");
-                }
-                //remove devices with low rssi that are likely to be unreachable soon. this
-                //mitigates ram overflow
-                //if(device.second<-70){
-                //    m_nearby_devices.erase(device.first);
-                //}
-            }*/
-           //for(auto const& device: m_nearby_devices){
-            //ESP_LOGD("RSSI","%d",(int)device.second);
-           //}
-            /*auto max=std::max_element(m_nearby_devices.begin(),m_nearby_devices.end(),[](pair_type const& a, pair_type const& b){
-                return a.second<b.second; //compare map hold values
-            });
-            /*std::for_each(m_nearby_devices_lastonline.begin(),m_nearby_devices_lastonline.end(),[this](pair_type const& device){
-                //check last update timestamp
-                if(abs(device.second-m_last_timestamp)>10){//if a device has not been updated for 10+ seconds
-                    m_nearby_devices.erase(device.first);
-                    m_nearby_devices_lastonline.erase(device.first);
-                }
-            });
 
-            return max->second>threshold;*/
             return true;
         }
 
         private:
-             map_device m_nearby_devices; /// Map MAC to rssi of nearby devices
-             map_device m_nearby_devices_lastonline; /// Map MAC to last detected state
-             uint16_t m_last_timestamp;
+             //map_device m_nearby_devices; /// Map MAC to rssi of nearby devices
+             //map_device m_nearby_devices_lastonline; /// Map MAC to last detected state
+             bleDevice m_devices[128];
+             bool m_scanComplete;
             
 
     };
